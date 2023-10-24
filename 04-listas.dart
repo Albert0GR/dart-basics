5a0e62a633407bd1fd28882bbdde6106f13643a2
Albert0GR <102188547+Albert0GR@users.noreply.github.com>
2023-10-24 06:57:55 -0600

    Update 04-listas.dart

--- 04-listas.dart
+++ 04-listas.dart
@@ -1,11 +1,16 @@
 void main() {
   
-  
-  List<int> numeros = [1,2,3,4,5,6,7,8,9,10];
-  numeros.add(11);
+  List? numerosnull = null; //lista  dinamica con valor de incializacion null
+  List<int> numeros2 = [12,13,14,15,16,17];
+  List<int> numeros = [1,2,3,4,5,6,7,8,9,10];  //lista acotada con valores enteros 
+  numeros.add(11);    //agrega un elemento a la lista 
+  numeros.addAll(numeros2); //agrega la lista en otra lista
   print( numeros );
   
-  final masNumeros = List.generate(100, (int index) => index );
+  final masNumeros = List.generate(100, (int i) => i);//lista con 100 elementos 
  
   print(masNumeros);
+  final masNumeros2 = List.generate(100, (int i) => (i)*2 -2);//lista con 100 elementos con salto de 2 e inicio en -2
+ 
+  print(masNumeros2);
 }
