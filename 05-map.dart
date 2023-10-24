b4433c2995b9bafb7ee24d1ad2eaa17e4bc4c3c8
Albert0GR <102188547+Albert0GR@users.noreply.github.com>
2023-10-24 10:17:51 -0600

    Update 05-map.dart

--- 05-map.dart
+++ 05-map.dart
@@ -1,21 +1,22 @@
 void main() {
-  
+  //debemos pensar que informacion almacena el mapa
 //   Map persona = {
-//     'nombre': 'Fernando',
-//     'edad': 35,
-//     'soltero': false,
+//     'nombre': 'Osita',
+//     'edad': 25,
+//     'casada': true,
 //     true: false,
 //     1: 100,
 //     2: 500
 //   };
   
-  Map<String, dynamic> persona = {
-    'nombre': 'Fernando',
-    'edad': 35,
-    'soltero': false,
+  Map<String, dynamic> persona = {    //tipos de datos especificados llave:'string', valores 'dynamic'
+    'nombre': 'osita',
+    'edad': 25,
+    'casada': true,
   };
   
-  persona.addAll({ 'segundoNombre': 'Juan' });
+  persona.addAll({ 'segundoNombre': 'Oso' });// añadir un nuevo elemento
+  print(persona['nombre']);  // para acceder a un dato 
   
   print( persona );
 }
