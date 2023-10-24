3764288d7d55d61086b82fe897100a1254be667a
Albert0GR <102188547+Albert0GR@users.noreply.github.com>
2023-10-24 10:31:22 -0600

    Update 06-funciones.dart

--- 06-funciones.dart
+++ 06-funciones.dart
@@ -1,22 +1,21 @@
 void main() {
   
-  final nombre = 'Fernando';
+  final nombre = 'Alberto';
   
-//   saludar( nombre, 'Greetings' );
-  saludar2( nombre: nombre, mensaje: 'Greetings' );
+   saludar( nombre, 'holaaa' );
+  saludar2( nombre: nombre, mensaje: 'Saludos2' );
   
 }
 
-void saludar( String nombre, [ String mensaje = 'Hi' ]) {
-  print('$mensaje $nombre');
+void saludar( String name, [ String mensaje = 'Sin saludo' ]) {  //al poner el argumento entre llaves indicamos que es un argumento opcional 
+  print('$mensaje $name');
 }
 
 
-void saludar2({ 
-  required String nombre, 
-  required String mensaje,
+void saludar2({     //aqui usamos otro tipo de argumentos ,  
+  required String nombre, //aqui especificamos el tipo de argumento a recibir , sin orden 
+  required String mensaje,  // si colocamos un signo de interrogacion el argumento puede ser opcional
  }) {
   print('$mensaje $nombre');
 }
-
 
